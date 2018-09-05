bb9abe23de405860b63bb1d5fdc2ac9b1816cc7c
C0D35TAR <cody.joel.tate@gmail.com>
2018-09-05 21:59:18 +1000

    2nd Commit - Pushed the wrong game hehe

---
+++ Assets/Scripts/Jump.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class Jump : MonoBehaviour
+{
+    public LayerMask groundLayers;
+
+    public float jumpForce = 7f;
+
+    public CapsuleCollider col;
+
+    private Rigidbody rb;
+    private PlayerController playerController;
+
+	void Start ()
+	{
+	    rb = GetComponent<Rigidbody>();
+	    col = GetComponent<CapsuleCollider>();
+	    playerController = GetComponent<PlayerController>();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate ()
+	{
+		PerformJump();
+	}
+
+    void PerformJump()
+    {
+        if (IsGrounded() && Input.GetButton("Jump"))
+        {
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+        }
+    }
+
+    private bool IsGrounded()
+    {
+        return Physics.CheckCapsule(col.bounds.center, new Vector3(col.bounds.center.x, col.bounds.min.y, col.bounds.center.z), col.radius * .9f, groundLayers);
+        
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(PlayerMotor))]
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private float speed = 5f;
+    [SerializeField] private float lookSensitivity = 3f;
+
+    //I'll use these later hehe
+    [Header("Jump Settings")]
+    public float fallMultiplier = 2.5f;
+    public float lowJumpMulitplier = 2f;
+
+    private PlayerMotor motor;
+    
+    void Start()
+    {
+        motor = GetComponent<PlayerMotor>();
+    }
+
+    void Update()
+    {
+        //Calculate movement velocity as a 3D vector
+        float _xMove = Input.GetAxisRaw("Horizontal");
+        float _zMove = Input.GetAxisRaw("Vertical");
+
+        Vector3 _moveHorizontal = transform.right * _xMove; //(1,0,0)
+        Vector3 _moveVeritcal = transform.forward * _zMove; //(0,0,1)
+
+        //Final movement vector
+        Vector3 _velocity = (_moveHorizontal + _moveVeritcal).normalized * speed;
+
+        //Apply movement
+        motor.Move(_velocity);
+
+        //Calculate rotation as a 3D vector (Turning Around)
+        float _yRot = Input.GetAxisRaw("Mouse X");
+
+        Vector3 _rotation = new Vector3(0f, _yRot, 0f) * lookSensitivity;
+
+        //Apply rotation
+        motor.Rotate(_rotation);
+
+        //Calculate camera rotation as a 3D vector (Turning Around)
+        float _xRot = Input.GetAxisRaw("Mouse Y");
+
+        Vector3 _camerarotation = new Vector3(_xRot, 0f, 0f) * lookSensitivity;
+
+        //Apply rotation
+        motor.RotateCamera(_camerarotation);
+
+       
+
+        //Apply jump
+       // motor.Jump(_jump);
+    }
+}
